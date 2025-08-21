eff2bf8cbf0739acb513bfd593f82e67c63dece8
lavroovich <168745434+lavroovich@users.noreply.github.com>
2025-08-21 17:04:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -114,4 +114,4 @@ with app.app_context():
     database.create_all()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 4400)))
