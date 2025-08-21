6a04dba3d3cfb46f0e67e4f6070487a050290c48
lavroovich <168745434+lavroovich@users.noreply.github.com>
2025-08-21 17:11:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 import json
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///vines.db"
